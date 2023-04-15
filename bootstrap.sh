aa5086f563eff5f438cd76091b863150c8260dd2
NEM-NE <dlatqdlatq@mju.ac.kr>
2023-04-15 20:30:54 +0900

    Add spacevim

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,9 @@ brew cask cleanup
 [ ! -f $HOME/.gitconfig-private ] && ln -nfs $HOME/dotfiles/.gitconfig $HOME/.gitconfig-private
 [ ! -f $HOME/.gitignore_global ] && ln -nfs $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
 
+# install space vim
+curl -sLf https://spacevim.org/install.sh | bash
+
 chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 [ ! -f $HOME/.zshrc ] && ln -nfs $HOME/dotfiles/.zshrc $HOME/.zshrc
